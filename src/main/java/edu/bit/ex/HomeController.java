f738db6bed852284c3b09a1ce6c4c8c34a821355
Yun DongHun <do81818@naver.com>
2021-06-09 14:34:19 +0900

    [Spring security] STEP 7 완료

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -21,29 +21,45 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
-	@GetMapping("/login/loginForm")
-	   public String loginForm() {
-	      log.info("Welcome Login Form");
-	      return "login/loginForm2";
-	   }
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        logger.info("Welcome home! The client locale is {}.", locale);
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
+    }
+
+    @GetMapping("/login/loginForm")
+    public String loginForm() {
+        log.info("Welcome Login Form");
+        return "login/loginForm2";
+    }
+
+    @GetMapping("/user/userHome")
+    public void userHome() {
+        log.info("userHome");
+    }
+
+    @GetMapping("/admin/adminHome")
+    public void adminHome() {
+        log.info("adminHome");
+    }
+
+    @GetMapping("/login/accessDenied")
+    public void accessDenied() {
+        log.info("Access is Denied");
+    }
+
 }
